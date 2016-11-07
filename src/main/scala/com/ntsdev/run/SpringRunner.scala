416168c59f8bdf4bbfcabcb3b20e0aa305986d40
Neil Shannon <nxs3535>
2016-11-07 17:35:52 -0500

    updating runner

--- src/main/scala/com/ntsdev/run/SpringRunner.scala
+++ src/main/scala/com/ntsdev/run/SpringRunner.scala
@@ -2,6 +2,7 @@ package com.ntsdev.run
 
 import com.ntsdev.config.{LocalGraphConfiguration, RemoteGraphConfiguration, ServiceConfig}
 import com.ntsdev.service.TestDataService
+import org.slf4j.LoggerFactory
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.{AnnotationConfigApplicationContext, ComponentScan, Configuration}
 
@@ -15,10 +16,14 @@ import org.springframework.context.annotation.{AnnotationConfigApplicationContex
 class SpringRunner
 
 object SpringRunner extends App with ServiceConfig {
+  val logger = LoggerFactory.getLogger(getClass)
+
   if("cloud" equals config.getString("SPRING_PROFILES_ACTIVE")) {
+    logger.info("Loading cloud configuration...")
     val context: ApplicationContext = new AnnotationConfigApplicationContext(classOf[RemoteGraphConfiguration])
   }
   else {
+    logger.info("Loading local configuration...")
     val context: ApplicationContext = new AnnotationConfigApplicationContext(classOf[LocalGraphConfiguration])
     val testDataService: TestDataService = context.getBean("testDataService").asInstanceOf[TestDataService]
     testDataService.loadTestData()
